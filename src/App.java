403b073f546d68fd19be024d6f36cfa2db03eea6
taito takara <e215717@ie.u-ryukyu.ac.jp>
2021-12-19 23:21:00 +0900

    step3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        String str = null;
+        try {
+        System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/report5.java
+++ src/report5.java
@@ -1,11 +1,8 @@
 public class report5 {
     public static void main(String[] args){
-        String str = null;
-        try {
-        System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+        
     }
 }
