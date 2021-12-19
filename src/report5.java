5fbf643b57626d8522223aa2d8d803f29d885209
taito takara <e215717@ie.u-ryukyu.ac.jp>
2021-12-19 20:46:10 +0900

    step2

--- src/report5.java
+++ src/report5.java
@@ -1,6 +1,11 @@
 public class report5 {
     public static void main(String[] args){
         String str = null;
+        try {
         System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
